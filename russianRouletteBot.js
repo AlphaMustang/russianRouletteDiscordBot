e9a630ed60804d227698457bdf4a61cb844f9c7c
alphamustang <ryan.noel@hotmail.com>
2021-05-01 17:40:45 +0100

    The code for the russian roulette bot.

---
+++ russianRouletteBot.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+  if (msg.content === "rr") {
+    if (!msg.guild) return;
+
+    if (msg.member.voice.channel) {
+      var connection = await msg.member.voice.channel.members.array();
+      var numberOfPeople = connection.length;
+      var randomNumber = Math.random();
+      console.log(randomNumber);
+      randomNumber = randomNumber * numberOfPeople;
+      randomNumber = Math.ceil(randomNumber) - 1;
+      var guildInfo = connection[randomNumber];
+      guildInfo.voice.kick("Testing")
+      .then(() => {
+        msg.reply("A user was kicked");
+      })
+      .catch(err => {
+        msg.reply("No user was kicked");
+      });
+    } else {
+      msg.reply("You need to join a voice channel first");
+    }
+  }
+});
+
+client.login('ODE0MjAxMjgxNjgwOTAwMTM4.YDaaCA.l2EtMmchcCJrEmT7lI_GO0XnE7w');
